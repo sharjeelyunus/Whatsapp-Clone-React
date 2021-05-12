593ebe44eaf7db4ff3ce190ea153f37b83fc32f3
sharjeelyunus <sharjeel924@gmail.com>
2021-05-13 04:48:57 +0500

    📦 NEW: Firebase ➕

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCEUydQPy6CC-0tZbaMulJ6Z_NzAiCa3rE",
+    authDomain: "whatsappreactclone0.firebaseapp.com",
+    projectId: "whatsappreactclone0",
+    storageBucket: "whatsappreactclone0.appspot.com",
+    messagingSenderId: "906370785250",
+    appId: "1:906370785250:web:d5ee7efd052eb1c330a3bb",
+    measurementId: "G-KPXBQV0WPC"
+};
