f8ae74a39c75709aa3a639e53dc8609108445e97
sharjeelyunus <sharjeel924@gmail.com>
2021-05-13 05:27:13 +0500

    📦 NEW:  Sidebar Header

---
+++ src/Sidebar.js
@@ -0,0 +1,31 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import React from 'react';
+import './Sidebar.css';
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                <Avatar />
+                <div className="sidebar__headerRight">
+                    <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="sidebar__search"></div>
+            <div className="sidebar__chats"></div>
+        </div>
+    )
+}
+
+export default Sidebar;
