919ad31c7bbc7c2ac2bf77ec57299abfb2375718
sharjeelyunus <sharjeel924@gmail.com>
2021-05-16 01:18:30 +0500

    📦 NEW: Chat, Chat Header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Sidebar from './Sidebar';
+import Chat from './Chat';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
     <div className="App">
       <div className="app__body">
         <Sidebar />
-        {/* Chat */}
+        <Chat />
       </div>
     </div>
   );
---
+++ src/Chat.js
@@ -0,0 +1,41 @@
+import { Avatar, IconButton } from '@material-ui/core';
+import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import React, { useEffect, useState } from 'react';
+import './Chat.css';
+
+function Chat() {
+    const [seed, setSeed] = useState('');
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random() * 5000));
+    }, []);
+
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`} />
+
+                <div className="chat__headerInfo">
+                    <h3>Room Name</h3>
+                    <p>Last Seen at ...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVert />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body"></div>
+            <div className="chat__footer"></div>
+        </div>
+    )
+}
+
+export default Chat;
