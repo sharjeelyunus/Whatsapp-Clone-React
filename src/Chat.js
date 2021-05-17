fdaa18180725885fad93c76bc4704fbad792cc99
sharjeelyunus <sharjeel924@gmail.com>
2021-05-18 01:02:41 +0500

    👌 IMPROVE: Chat Footer

--- src/Chat.js
+++ src/Chat.js
@@ -1,14 +1,23 @@
 import { Avatar, IconButton } from '@material-ui/core';
 import { AttachFile, MoreVert, SearchOutlined } from '@material-ui/icons';
+import MicIcon from '@material-ui/icons/Mic';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import React, { useEffect, useState } from 'react';
 import './Chat.css';
 
 function Chat() {
+    const [input, setInput] = useState("");
     const [seed, setSeed] = useState('');
 
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000));
     }, []);
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        console.log(input);
+        setInput("");
+    };
 
     return (
         <div className="chat">
@@ -39,7 +48,14 @@ function Chat() {
                     <span className="chat__timestamp">2:37</span>
                 </p>
             </div>
-            <div className="chat__footer"></div>
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input value={input} onChange={e => setInput(e.target.value)} placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit">Send a message</button>
+                </form>
+                <MicIcon />
+            </div>
         </div>
     )
 }
