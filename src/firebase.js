308fbee2c65229bb24298bed4e54b84ac28d1c95
sharjeelyunus <sharjeel924@gmail.com>
2021-05-19 01:18:13 +0500

    📦 NEW: Firebase Config

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyCEUydQPy6CC-0tZbaMulJ6Z_NzAiCa3rE",
@@ -8,3 +10,11 @@ const firebaseConfig = {
     appId: "1:906370785250:web:d5ee7efd052eb1c330a3bb",
     measurementId: "G-KPXBQV0WPC"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
